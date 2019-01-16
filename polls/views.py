3920318138b9ac18064abd9418947885d4b4b4fd
Tolkimon <axl.parangan@gmail.com>
2019-01-16 22:35:13 +0800

    wew

--- polls/views.py
+++ polls/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.urls import path, include
 from .models import Question
+from .forms import QuestionModelsForm
 
 # Create your views here.
 
@@ -16,3 +17,23 @@ def help(request):
     return HttpResponse('This is help page')    
 
 
+def detail(request, question_id):
+    context = {}
+    context['question'] = Question.objects.get(id = question_id)
+    return render(request, 'details.html', context)
+
+
+def update(request, question_id):
+    question = Question.objects.get(id=question_id)
+    context = {}
+    if request.method == 'POST':
+        form = QuestionModelsForm(request.POST, instance=question)
+        if form.is_valid():
+            form.save()
+            return HttpResponse('Question updated')
+        else:
+            context['form'] = form
+            render(request, 'update.html', context)
+    else:
+        context['form'] = QuestionModelsForm(instance=question)
+        return render(request, 'update.html', context)
